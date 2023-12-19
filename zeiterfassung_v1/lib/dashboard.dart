d5eb4daa8ee954397acd14031f2669c454bcc642
Janis0001 <janis.papai@htl-saalfelden.at>
2023-12-19 11:21:22 +0100

    Create dashboard.dart
    Simple_Buchungen_v1

---
+++ zeiterfassung_v1/lib/dashboard.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Homepage> {
+  final TextEditingController _timeController = TextEditingController();
+
+  DateTime now = DateTime.now();
+  late String _formatted = DateFormat('hh:mm').format(now);
+  List<String> dropdownItems = ['Normale Buchung', 'Dienstgang', 'Homeoffice'];
+  String selectedDropdownItem = 'Normale Buchung';
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 180.0),
+              // Display current time
+              Text(
+                _formatted,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 50.0),
+              ),
+              SizedBox(height: 20.0), // Add some spacing
+              // Checkbox and text
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    },
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    'Buchungsende',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              // Dropdown menu
+              if (isChecked==false)
+                Center(
+                  child: DropdownButton<String>(
+                    value: selectedDropdownItem,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedDropdownItem = newValue!;
+                      });
+                    },
+                    items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              Spacer(),
+              // "Buchen" Button
+              ElevatedButton(
+                onPressed: () {
+                },
+                child: Text('Buchen'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
